e5618ee002d9b676f6fc7c26e152d001665d9434
Byron Katz <byronka@msn.com>
2014-02-01 12:37:30 -0500

    adjusting tabs and spaces only - no real changes

--- capsaicin_server.c
+++ capsaicin_server.c
@@ -14,7 +14,7 @@
 
 #define SERVER_PORT_NUMBER 4321
 
- /* size of messages for managing communication */
+/* size of messages for managing communication */
 #define CTRL_MSG_SIZE 16 
 #define OK_MSG 0
 #define ACTION_CLIENT_WANTS_VIDEO 1
@@ -34,165 +34,164 @@ extern int run_server();
 int
 read_from_stream_into_file(int socket, int file) {
 #ifdef DEBUG
-	fprintf(stdout, "receiving file id[%d] from socket id [%d]\n",
-			file, socket);
+  fprintf(stdout, "receiving file id[%d] from socket id [%d]\n",
+	  file, socket);
 #endif
-	ssize_t max_buf_size = 1024;
-	unsigned char buf[max_buf_size];
-	int result;
-	do {
-		memset(buf, 0, max_buf_size);
-		result = Read(socket, buf, max_buf_size);
-		if (result > 0) { /* we read some bytes */
+  ssize_t max_buf_size = 1024;
+  unsigned char buf[max_buf_size];
+  int result;
+  do {
+    memset(buf, 0, max_buf_size);
+    result = Read(socket, buf, max_buf_size);
+    if (result > 0) { /* we read some bytes */
 #ifdef DEBUG
-			fprintf(stderr, "putting %d bytes into file\n", result);
+      fprintf(stderr, "putting %d bytes into file\n", result);
 #endif
-			Write(file, buf, result);
-		}
-		if (result < 0) { /* -1 means general error */
-				perror("read socket error");
-		 /* TODO - BK - 1/9/2014 - do we need a better way to exit? */
-				exit(13); 
-		}
-	} while(result != 0);  /* returning 0 means no more bytes to write */
+      Write(file, buf, result);
+    }
+    if (result < 0) { /* -1 means general error */
+      perror("read socket error");
+      /* TODO - BK - 1/9/2014 - do we need a better way to exit? */
+      exit(13); 
+    }
+  } while(result != 0);  /* returning 0 means no more bytes to write */
 #ifdef DEBUG
-	fprintf(stderr, "closing the file\n");
+  fprintf(stderr, "closing the file\n");
 #endif
-	Close(file);
-	return 0; /* TODO - BK no return info implemented */
+  Close(file);
+  return 0; /* TODO - BK no return info implemented */
 }
 
 int
 send_requested_file(int socket, int id) {
-	int myfile;
-	char pathbuf[128];
-	sprintf(pathbuf, VIDEO_PATH_BASE ,id);
+  int myfile;
+  char pathbuf[128];
+  sprintf(pathbuf, VIDEO_PATH_BASE ,id);
 
 #ifdef DEBUG
-	fprintf(stderr, "\nSending requested file id[%d] to socket id [%d]\n",
-			id, socket);
-	fprintf(stderr, "\nabout to open file at %s", pathbuf);
+  fprintf(stderr, "\nSending requested file id[%d] to socket id [%d]\n",
+	  id, socket);
+  fprintf(stderr, "\nabout to open file at %s", pathbuf);
 #endif
 
-	myfile = Open(pathbuf, O_RDONLY | O_NONBLOCK);
+  myfile = Open(pathbuf, O_RDONLY | O_NONBLOCK);
 
 #ifdef DEBUG
-	fprintf(stderr, "\nSending file at %s to socket %d", pathbuf, socket);
+  fprintf(stderr, "\nSending file at %s to socket %d", pathbuf, socket);
 #endif
 
   int sbytes = 0;
-	int origin = 0;
-	int nbytes = 1024;
-	sbytes = sendfile_wrapper(myfile, socket, origin, nbytes);
+  int origin = 0;
+  int nbytes = 1024;
+  sbytes = sendfile_wrapper(myfile, socket, origin, nbytes);
 
 #ifdef DEBUG
-	fprintf(stderr, 
-			"\nSuccessfully sent file at "
-			"%s to socket %d, (%d bytes)", pathbuf, socket, sbytes);
+  fprintf(stderr, 
+	  "\nSuccessfully sent file at "
+	  "%s to socket %d, (%d bytes)", pathbuf, socket, sbytes);
 #endif
 
-	return 0;
+  return 0;
 }
 
 int
 receive_file(int socket, int id) {
-	unsigned char msg = ACTION_TELL_CLIENT_SERVER_IS_READY;
-	Write(socket, &msg, sizeof(&msg));
+  unsigned char msg = ACTION_TELL_CLIENT_SERVER_IS_READY;
+  Write(socket, &msg, sizeof(&msg));
 #ifdef DEBUG
-	fprintf(stderr, "telling client that server is ready to receive file\n");
+  fprintf(stderr, "telling client that server is ready to receive file\n");
 #endif
 
-	/* receive file */
+  /* receive file */
 
-	char pathbuf[128];
-	sprintf(pathbuf, VIDEO_PATH_BASE,id);
-	int file = Open(pathbuf, O_RDWR | O_CREAT);
-	read_from_stream_into_file(socket, file);
-	return 0;
+  char pathbuf[128];
+  sprintf(pathbuf, VIDEO_PATH_BASE,id);
+  int file = Open(pathbuf, O_RDWR | O_CREAT);
+  read_from_stream_into_file(socket, file);
+  return 0;
 }
 
 int
 handle_accepted_socket(int socket) {
-	unsigned char buf[CTRL_MSG_SIZE];
-	Read(socket, buf, CTRL_MSG_SIZE);
+  unsigned char buf[CTRL_MSG_SIZE];
+  Read(socket, buf, CTRL_MSG_SIZE);
 
-	/*
+  /*
 
-		 unencrypted packet size = 4 + 1 + 8 + 8 + 8 + 4 = 33 bytes
+    unencrypted packet size = 4 + 1 + 8 + 8 + 8 + 4 = 33 bytes
 
-|---------|--------|-----------|-----------|--------|---------|
-| 				|        |           |           |        |         |
-|sessionid|action  | entity id | userid    | offset | sbytes  |
-|  4 bytes|1 byte  | 8  bytes  | 8  bytes  |8 bytes | 4 bytes |
-|---------|--------|-----------|-----------|--------|---------|
+    |---------|--------|-----------|-----------|--------|---------|
+    |sessionid|action  | entity id | userid    | offset | sbytes  |
+    |  4 bytes|1 byte  | 8  bytes  | 8  bytes  |8 bytes | 4 bytes |
+    |---------|--------|-----------|-----------|--------|---------|
 
-Reasoning:
-- I will almost certainly have fewer than 256 specific actions
-- the offset needs to be of sufficient size to place a pointer anywhere
-	within a file.  Therefore it needs to be as large as the largest
-	file I could expect to see.
-- sbytes - cannot be much larger than 10kilobytes.  
-	*/
+    Reasoning:
+    - I will almost certainly have fewer than 256 specific actions
+    - the offset needs to be of sufficient size to place a pointer anywhere
+    within a file.  Therefore it needs to be as large as the largest
+    file I could expect to see.
+    - sbytes - cannot be much larger than 10kilobytes.  
+  */
 
-    int action = buf[0];
-    int id = 0;
+  int action = buf[0];
+  int id = 0;
 
-	switch(action) {
-		case ACTION_CLIENT_WANTS_VIDEO:
-			id = buf[1];
-			int nbytes = buf[2];
-			int offset = buf[3];
-			send_requested_file(socket, id);
-			break;
-		case ACTION_CLIENT_SENDING_SERVER_VIDEO:
-			id = buf[1];
-			receive_file(socket, id);
-			break;
-		default:
-			fprintf(stderr, "error: no action sent. buffer[0] is %u and "
-					"buffer[1] is %u. aborting\n", buf[0], buf[1]);
-			exit(12);
-	}
+  switch(action) {
+  case ACTION_CLIENT_WANTS_VIDEO:
+    id = buf[1];
+    int nbytes = buf[2];
+    int offset = buf[3];
+    send_requested_file(socket, id);
+    break;
+  case ACTION_CLIENT_SENDING_SERVER_VIDEO:
+    id = buf[1];
+    receive_file(socket, id);
+    break;
+  default:
+    fprintf(stderr, "error: no action sent. buffer[0] is %u and "
+	    "buffer[1] is %u. aborting\n", buf[0], buf[1]);
+    exit(12);
+  }
 	
-	Close(socket);
-	return 0;
+  Close(socket);
+  return 0;
 }
 
 int
 fork_server(int s, struct sockaddr_in sa) {
-	socklen_t size_var;
-	int accepted_socket;
-    switch (fork()) {
-        case -1:
-            perror("fork");
-            exit(3);
-            break;
-        default:
-            Close(s);
-            return 0;
-            break;
-        case 0:
-            break;
-    }
+  socklen_t size_var;
+  int accepted_socket;
+  switch (fork()) {
+  case -1:
+    perror("fork");
+    exit(3);
+    break;
+  default:
+    Close(s);
+    return 0;
+    break;
+  case 0:
+    break;
+  }
 
-    Listen(s, BACKLOG);
+  Listen(s, BACKLOG);
 
-    for (;;) {
-        size_var = sizeof sa;
-        accepted_socket = Accept(s, (struct sockaddr *)&sa, &size_var);
-		handle_accepted_socket(accepted_socket);
-    }
+  for (;;) {
+    size_var = sizeof sa;
+    accepted_socket = Accept(s, (struct sockaddr *)&sa, &size_var);
+    handle_accepted_socket(accepted_socket);
+  }
 }
 
 
 int run_server() {
-	int primary_socket;
-	struct sockaddr_in sa;
-	primary_socket = Socket(PF_INET, SOCK_STREAM, 0);
-	sa = prepareSocketAddress(sa, SERVER_PORT_NUMBER);
-	Bind(primary_socket, (struct sockaddr *)&sa, sizeof sa);
-	fork_server(primary_socket, sa);
-	return 0;   /* no return code implemented */
+  int primary_socket;
+  struct sockaddr_in sa;
+  primary_socket = Socket(PF_INET, SOCK_STREAM, 0);
+  sa = prepareSocketAddress(sa, SERVER_PORT_NUMBER);
+  Bind(primary_socket, (struct sockaddr *)&sa, sizeof sa);
+  fork_server(primary_socket, sa);
+  return 0;   /* no return code implemented */
 }
 
 
