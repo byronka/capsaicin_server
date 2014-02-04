fdc4ffc56996e0ad0c2ef984ca9262ab1efd456d
Byron Katz <byronka@msn.com>
2014-02-04 07:41:05 -0500

    Another step in removing header files.

--- utils/ntwk_utils.c
+++ utils/ntwk_utils.c
@@ -1,12 +1,12 @@
 #include <netinet/in.h>
 #include <stdio.h>
 
-void
+extern void
 print_ip_addr_32_bit (unsigned char* client_addr_c) {
 	fprintf(stderr, "%u.%u.%u.%u\n", client_addr_c[0],client_addr_c[1],client_addr_c[2],client_addr_c[3]);
 }
 
-unsigned char*
+extern unsigned char*
 convert_ip_addr (uint32_t number) {
 	static unsigned char finished_version[4];
 	finished_version[3] = number % 256;
